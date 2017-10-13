b0877dc5a20ea799fc44620ddbd71e1938bc3f6e
timknapp12 <timknapp12@gmail.com>
2017-10-12 21:12:23 -0600

    first commit

---
+++ controller.js
@@ -0,0 +1,11 @@
+module.exports = {
+    read: (req, res, next) => {
+        const dbInstance = req.app.get('db');
+
+        dbInstance.get_planes([25])
+        .then(planes => res.status(200).send(planes))
+        .catch( err => {
+            console.log(err);
+            res.status(500).send(err)});
+    }
+}
